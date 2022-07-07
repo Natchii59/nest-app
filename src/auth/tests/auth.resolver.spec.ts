d52a273282cc7bfd6edb62640dd62e8c03108467
Nathan Caron <caron.nathan@hotmail.com>
2022-07-07 19:47:10 +0200

    Add Tests for Auth module

--- src/auth/tests/auth.resolver.spec.ts
+++ src/auth/tests/auth.resolver.spec.ts
@@ -1,19 +1,82 @@
 import { Test, TestingModule } from '@nestjs/testing';
 
 import { AuthResolver } from '../auth.resolver';
+import { AuthService } from '../auth.service';
+import { AuthLoginArgs } from '../dto/auth-login.dto';
+import { LocalAuthGuard } from '../guards/local-auth.guard';
+import { JwtAuthGuard } from '../guards/jwt-auth.guard';
+import { jwtPayloadMock, userMock } from '../../../test/mocks/user.mock';
 
 describe('AuthResolver', () => {
   let resolver: AuthResolver;
+  let authService: AuthService;
+
+  const authServiceMock = {
+    login: jest.fn(() => ({
+      accessToken: 'ACCCESS_TOKEN',
+    })),
+  };
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [AuthResolver],
+      providers: [
+        AuthResolver,
+        {
+          provide: AuthService,
+          useValue: authServiceMock,
+        },
+      ],
     }).compile();
 
     resolver = module.get<AuthResolver>(AuthResolver);
+    authService = module.get<AuthService>(AuthService);
+    jest.clearAllMocks();
   });
 
   it('should be defined', () => {
     expect(resolver).toBeDefined();
+    expect(authService).toBeDefined();
+  });
+
+  describe('Login', () => {
+    it('should return accessToken of logged user', async () => {
+      const args: AuthLoginArgs = {
+        email: 'test@gmail.com',
+        password: 'testy',
+      };
+
+      const result = await resolver.authLogin(userMock, args);
+
+      expect(authServiceMock.login).toBeCalledWith(userMock);
+      expect(result.accessToken).toEqual(expect.any(String));
+    });
+
+    it('should have the guard LocalAuthGuard', () => {
+      const guards = Reflect.getMetadata(
+        '__guards__',
+        AuthResolver.prototype.authLogin,
+      );
+      const guard = new guards[0]();
+
+      expect(guard).toBeInstanceOf(LocalAuthGuard);
+    });
+  });
+
+  describe('Profile', () => {
+    it('should return payload of current user', async () => {
+      const result = await resolver.getProfile(jwtPayloadMock);
+
+      expect(result).toEqual(jwtPayloadMock);
+    });
+
+    it('should have the guard JwtAuthGuard', () => {
+      const guards = Reflect.getMetadata(
+        '__guards__',
+        AuthResolver.prototype.getProfile,
+      );
+      const guard = new guards[0]();
+
+      expect(guard).toBeInstanceOf(JwtAuthGuard);
+    });
   });
 });
--- src/auth/tests/auth.service.spec.ts
+++ src/auth/tests/auth.service.spec.ts
@@ -1,19 +1,103 @@
+import { JwtService } from '@nestjs/jwt';
 import { Test, TestingModule } from '@nestjs/testing';
+import * as bcrypt from 'bcrypt';
 
 import { AuthService } from '../auth.service';
+import { UserService } from '../../user/user.service';
+import { jwtPayloadMock, userMock } from '../../../test/mocks/user.mock';
 
 describe('AuthService', () => {
   let service: AuthService;
+  let userService: UserService;
+  let jwtService: JwtService;
+
+  const userServiceMock = {
+    getByEmail: jest.fn(() => userMock),
+  };
+
+  const jwtServiceMock = {
+    sign: jest.fn(() => 'ACCESS_TOKEN'),
+  };
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [AuthService],
+      providers: [
+        AuthService,
+        {
+          provide: UserService,
+          useValue: userServiceMock,
+        },
+        {
+          provide: JwtService,
+          useValue: jwtServiceMock,
+        },
+      ],
     }).compile();
 
     service = module.get<AuthService>(AuthService);
+    userService = module.get<UserService>(UserService);
+    jwtService = module.get<JwtService>(JwtService);
   });
 
   it('should be defined', () => {
     expect(service).toBeDefined();
+    expect(userService).toBeDefined();
+    expect(jwtService).toBeDefined();
+  });
+
+  describe('Validate User', () => {
+    it('should return validated user', async () => {
+      const email = 'test@gmail.com';
+      const password = 'testy';
+
+      const compareSyncSpy = jest.spyOn(bcrypt, 'compareSync');
+      compareSyncSpy.mockReturnValueOnce(true);
+
+      const result = await service.validate(email, password);
+
+      expect(userServiceMock.getByEmail).toBeCalledWith(email);
+      expect(compareSyncSpy).toBeCalledWith(password, userMock.password);
+
+      // eslint-disable-next-line @typescript-eslint/no-unused-vars
+      const { password: userPassword, ...checkResult } = userMock;
+      expect(result).toEqual(checkResult);
+    });
+
+    it('should return unvalidated user with wrong password', async () => {
+      const email = 'test@gmail.com';
+      const password = 'testy';
+
+      const compareSyncSpy = jest.spyOn(bcrypt, 'compareSync');
+      compareSyncSpy.mockReturnValueOnce(false);
+
+      const result = await service.validate(email, password);
+
+      expect(userServiceMock.getByEmail).toBeCalledWith(email);
+      expect(compareSyncSpy).toBeCalledWith(password, userMock.password);
+
+      expect(result).toBeNull();
+    });
+
+    it('should return unvalidated user with wrong email', async () => {
+      const email = 'test@gmail.com';
+      const password = 'testy';
+
+      jest.spyOn(userService, 'getByEmail').mockReturnValueOnce(null);
+
+      const result = await service.validate(email, password);
+
+      expect(userServiceMock.getByEmail).toBeCalledWith(email);
+
+      expect(result).toBeNull();
+    });
+  });
+
+  describe('Login User', () => {
+    it('should return accessToken of payload user', () => {
+      const result = service.login(userMock);
+
+      expect(jwtServiceMock.sign).toBeCalledWith(jwtPayloadMock);
+      expect(result.accessToken).toEqual(expect.any(String));
+    });
   });
 });
